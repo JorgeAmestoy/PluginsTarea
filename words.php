48b73a810f3ee0c92d2dac142026d2a73a55e62e
jorgeamestoy <jamestoysabucedo@danielcastelao.org>
2023-12-11 14:21:34 +0100

    actualizo examen

--- words.php
+++ words.php
@@ -11,34 +11,86 @@ Author: Jorge Amestoy
 Version: 1.0.0
 Author URI: http://mjorge.amestoy/
 */
-
+/*
 function inicioPlugin(){
     createTable();
     insertData();
-}
+}*/
 #Se ejecutará la base de datos cuando se cargue el plugin, y en la base de datos
 #de la derecha saldrá un wp_palabras
 /**
  * Carga tabla wp_dam
  * Con las palabras que queremos cambiar
  */
+
 function createTable(){
     global $wpdb;
-    $table_name = $wpdb->prefix . 'palabras';
+    $table_name = $wpdb->prefix . 'mayusculas';
     $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
-        word varchar(255) NOT NULL,
-        antonym varchar(255) NOT NULL,
+        originalText varchar(255) NOT NULL,
+        uppercaseText varchar(255) NOT NULL,
         PRIMARY KEY  (id)
     );";
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
 }
+add_action('plugins_loaded','createTable');
 
 
-/**
- * Inserta palabras en la tabla wp_dam
-*/
+
+function filtrarTitulo ($titulo){
+    $titulo = almacenar($titulo);
+    return $titulo;
+}
+
+function filtrarContenido($contenido){
+    $contenido = almacenar($contenido);
+    return $contenido;
+}
+
+//add_filter('the_title','filtrarTitulo');
+//add_filter('the_content','filtrarContenido');
+
+function almacenar($texto){
+    $textoFiltrado = strtoupper($texto);
+    global $wpdb;
+    $wpdb -> insert(
+        $wpdb->prefix . 'mayusculas',
+        array(
+            'originalText' => $texto,
+            'uppercaseText' => $textoFiltrado
+        )
+    );
+
+}
+
+function obtenerContenidoTabla() {
+    global $wpdb;
+
+    $resultados = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}mayusculas", ARRAY_A);
+
+    // Devolver los resultados
+    return $resultados;
+}
+
+function renym_wordpress_typo_fix( $text ) {
+    $obtener = obtenerContenidoTabla();
+    foreach ($obtener as $key => $value) {
+        $text = str_replace( $value['originalText'], $value['uppercaseText'], $text );
+    }
+    return $text;
+}
+add_filter( 'the_content', 'renym_wordpress_typo_fix' );
+
+
+
+
+
+
+
+
+/*
 function insertData(){
     global $wpdb;
     $table_name = $wpdb->prefix . 'palabras';
@@ -85,24 +137,16 @@ function insertData(){
 }
 
 add_action('plugins_loaded','inicioPlugin');
+*/
 
-
-/**
- * @param $text
- * @return array|string|string[]
- */
+/*
 function renym_wordpress_typo_fix( $text ) {
     return str_replace( 'WordPress', 'WordPressDAM', $text );
 }
 
 add_filter( 'the_content', 'renym_wordpress_typo_fix' );
+*/
 
-
-/**
- * Cambia palabras usando arrays
- * @param $text
- * @return array|string|string[]
- */
 /*
 function renym_words_replace($text){
 $palabras = array("guapo","largo","gordo","alto","dulce");
@@ -112,11 +156,7 @@ $antonimos = array("feo","corto","flaco","bajo","salado");
 add_filter( 'the_content', 'renym_words_replace' );
 */
 
-/**
- * Cambia palabras usando base de datoss
- * @param $text
- * @return array|string|string[]
- */
+/*
 function renym_words_replace_db($text){
     global $wpdb;
     $table_name = $wpdb->prefix . 'palabras';
@@ -128,3 +168,4 @@ function renym_words_replace_db($text){
 }
 add_filter( 'the_content', 'renym_words_replace_db' );
 
+*/
